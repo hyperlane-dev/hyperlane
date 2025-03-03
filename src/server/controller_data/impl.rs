5dcb25b820d5f40fe8bf842e3361845dbbb25e25
尤雨东 <root@ltpp.vip>
2025-03-03 21:43:32 +0800

    feat: v4.19.1

--- src/server/controller_data/impl.rs
+++ src/server/controller_data/impl.rs
@@ -78,13 +78,17 @@ impl ArcRwLockControllerData {
     #[inline]
     pub async fn get_socket_host(&self) -> OptionSocketHost {
         let addr: Option<SocketAddr> = self.get_socket_addr().await;
-        addr.map(|socket_addr| socket_addr.ip())
+        let socket_host_opt: OptionSocketHost =
+            addr.map(|socket_addr: SocketAddr| socket_addr.ip());
+        socket_host_opt
     }
 
     #[inline]
     pub async fn get_socket_port(&self) -> OptionSocketPort {
         let addr: Option<SocketAddr> = self.get_socket_addr().await;
-        addr.map(|socket_addr| socket_addr.port())
+        let socket_port_opt: OptionSocketPort =
+            addr.map(|socket_addr: SocketAddr| socket_addr.port());
+        socket_port_opt
     }
 
     #[inline]
