f99e25e589f54dca028b609dcf9d308a269b5565
尤雨东 <root@ltpp.vip>
2025-05-30 08:06:02 +0800

    feat: v5.9.0

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -4,6 +4,6 @@ pub(crate) mod r#trait;
 pub(crate) mod r#type;
 
 pub use r#trait::*;
+pub use r#type::*;
 
 pub(crate) use r#fn::*;
-pub(crate) use r#type::*;
--- src/handler/type.rs
+++ src/handler/type.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-pub(crate) type ArcFunc = Arc<dyn Func>;
-pub(crate) type VecArcFunc = Vec<ArcFunc>;
-pub(crate) type ArcErrorHandle = Arc<dyn ErrorHandle + Send + Sync + 'static>;
-pub(crate) type PinBoxFutureSend = Pin<Box<(dyn Future<Output = ()> + Send + 'static)>>;
+pub type ArcFunc = Arc<dyn Func>;
+pub type VecArcFunc = Vec<ArcFunc>;
+pub type ArcErrorHandle = Arc<dyn ErrorHandle + Send + Sync + 'static>;
+pub type PinBoxFutureSend = Pin<Box<(dyn Future<Output = ()> + Send + 'static)>>;
