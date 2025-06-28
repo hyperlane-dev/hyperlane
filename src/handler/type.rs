96da67b7e46bb74e895b1212049cf0cdb78a7018
尤雨东 <root@ltpp.vip>
2025-06-28 16:19:45 +0800

    feat: speed

--- src/handler/type.rs
+++ src/handler/type.rs
@@ -6,5 +6,4 @@ pub type VecArcFnPinBoxSendSync = Vec<ArcFnPinBoxSendSync>;
 pub type ArcErrorHandler = Arc<dyn ErrorHandler + Send + Sync + 'static>;
 pub type PinBoxFutureSend = Pin<Box<(dyn Future<Output = ()> + Send + 'static)>>;
 pub type ArcRwLockVecArcFnPinBoxSendSync = ArcRwLock<VecArcFnPinBoxSendSync>;
-pub type RwLockReadGuardVecArcFnPinBoxSendSync<'a> =
-    RwLockReadGuard<'a, Vec<Arc<dyn FnPinBoxSendSync>>>;
+pub type RwLockReadGuardVecArcFnPinBoxSendSync<'a> = RwLockReadGuard<'a, VecArcFnPinBoxSendSync>;
