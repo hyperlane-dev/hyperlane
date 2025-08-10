dc52387544a73c15bf411fbecc67e4e2d927dc2a
尤雨东 <root@ltpp.vip>
2025-08-10 22:40:58 +0800

    feat: v6.5.0

--- src/config/struct.rs
+++ src/config/struct.rs
@@ -7,38 +7,17 @@ use crate::*;
 #[derive(Clone, Data, CustomDebug, DisplayDebug, PartialEq, Eq, Deserialize, Serialize)]
 pub struct ServerConfig {
     /// The host address the server will bind to.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) host: String,
     /// The port number the server will listen on.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) port: usize,
     /// The buffer size for WebSocket connections.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) ws_buffer: usize,
     /// The buffer size for HTTP connections.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) http_buffer: usize,
     /// The TCP_NODELAY option for sockets.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) nodelay: OptionBool,
     /// The SO_LINGER option for sockets.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) linger: OptionDuration,
     /// The IP_TTL option for sockets.
-    #[get(pub(crate))]
-    #[get_mut(pub(crate))]
-    #[set(pub(crate))]
     pub(super) ttl: OptionU32,
 }
