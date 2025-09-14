f90a128859b4ed0b9eafe4c4e334226264c8f035
尤雨东 <root@ltpp.vip>
2025-09-14 20:10:03 +0800

    feat: v9.4.2

--- src/tests/server.rs
+++ src/tests/server.rs
@@ -21,7 +21,7 @@ async fn server() {
     async fn send_body_hook(ctx: Context) {
         let body: ResponseBody = ctx.get_request_body().await;
         if ctx.get_request().await.is_ws() {
-            let body_list: Vec<ResponseBody> = WebSocketFrame::create_frame_list(body);
+            let body_list: Vec<ResponseBody> = WebSocketFrame::create_frame_list(&body);
             ctx.send_body_list_with_data(&body_list).await.unwrap();
         } else {
             ctx.send_body().await.unwrap();
