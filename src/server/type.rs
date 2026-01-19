07d1ba5665ee3b1444268a95d14f2bfbb9f28526
尤雨东 <root@ltpp.vip>
2026-01-19 11:59:41 +0800

    feat: v10.35.4-test.1

--- src/server/type.rs
+++ src/server/type.rs
@@ -1,10 +1,5 @@
 use crate::*;
 
-/// A type alias for shared server state.
-///
-/// This is the core mechanism for sharing server state across threads.
-pub(crate) type SharedServerState = ArcRwLock<ServerInner>;
-
 /// A type alias for shared server configuration.
 ///
 /// This is the core mechanism for sharing server config state across threads.
