02faf511ded7906579eede96298fbad6cca89642
尤雨东 <root@ltpp.vip>
2026-01-21 23:42:54 +0800

    feat: v11.0.6

--- src/config/test.rs
+++ src/config/test.rs
@@ -5,23 +5,8 @@ async fn server_config_from_json() {
     let config_str: &'static str = r#"
         {
             "host": "0.0.0.0",
-            "port": 80,           
-            "request_config": {
-                "buffer_size": 8192,
-                "max_request_line_length": 8192,
-                "max_path_length": 8192,
-                "max_query_length": 8192,
-                "max_header_line_length": 8192,
-                "max_header_count": 100,
-                "max_header_key_length": 8192,
-                "max_header_value_length": 8192,
-                "max_body_size": 2097152,
-                "max_ws_frame_size": 65536,
-                "max_ws_frames": 6000,
-                "http_read_timeout_ms": 6000,
-                "ws_read_timeout_ms": 1800000
-            },
-            "nodelay": true,            
+            "port": 80,
+            "nodelay": true,
             "ttl": 64
         }
     "#;
